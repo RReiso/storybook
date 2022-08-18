1173d0af66bbc66cec968562bd7c053c2add2dc4
Ruta Reisoglu <rutareiso@protonmail.com>
2022-08-18 13:39:13 -0400

    Add storyName and a collection of stories

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import React from "react";
 
 export default {
-  title: "Button",
+  title: "form/Button",
   component: Button,
 };
 
@@ -10,3 +10,5 @@ export const Primary = () => <Button variant="primary">Primary</Button>;
 export const Secondary = () => <Button variant="secondary">Secondary</Button>;
 export const Success = () => <Button variant="success">Success</Button>;
 export const Danger = () => <Button variant="danger">Danger</Button>;
+
+Primary.storyName = "Primary button";
---
+++ src/components/Collection/Collection.stories.jsx
@@ -0,0 +1,12 @@
+import { Secondary, Danger } from "../Button/Button.stories";
+
+export default {
+  title: "form/Collection",
+};
+
+export const ButtonCollection = () => (
+  <>
+    <Secondary />
+    <Danger />
+  </>
+);
